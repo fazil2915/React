977a85a1c7182bf0631461b89ff5025de2919699
fazil <fazil2915@gmail.com>
2021-09-25 20:49:08 +0530

    multiple components

---
+++ 01-starting-setup/src/components/ExpenseDate.js
@@ -0,0 +1,19 @@
+import './ExpenseDate.css'
+
+
+function ExpenseDate(props){
+const date=props.date.toLocaleString('en-US',{ month:'long' });
+const year=props.date.getFullYear();
+const month=props.date.toLocaleString('en-US',{ day:'2-digit'});
+
+return (
+    <div className="expense-date">
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}</div>
+        <div className="expense-date__date">{date}</div>
+       </div>
+        
+    
+        );
+}
+export default ExpenseDate;
